23c4153d5035836a34edea90852ccf793cdbf8af
DanielNoveron <81577034+DanielNoveron@users.noreply.github.com>
2021-04-11 14:25:49 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// @ts-ignore
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBLi9C0wFwgSWlAe0Jhpv3PlTHHLJBxFE0",
+  authDomain: "pdgniotpwa.firebaseapp.com",
+  projectId: "pdgniotpwa",
+  storageBucket: "pdgniotpwa.appspot.com",
+  messagingSenderId: "1059269536666",
+  appId: "1:1059269536666:web:92a514d6ace0dc3e8e2823"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
