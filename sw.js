2c2a4c6ae942465348406192cd927f23b7cd1a24
DanielNoveron <81577034+DanielNoveron@users.noreply.github.com>
2021-04-12 18:41:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -54,58 +54,51 @@ const ARCHIVOS = [
 ];
 
 
-self.addEventListener("install",
-  evt => {
-    console.log("sw instalado.");
-    /* Realiza la instalación.
-     * Carga los archivos
-     * requeridos en la caché. */
-    // @ts-ignore
-    evt.waitUntil(cargaCache());
-  });
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => {
+        return cache.addAll(ARCHIVOS)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
 
 //cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-//Responder ya sea con el objeto en caché o continuar y buscar la url real
-e.respondWith(
-caches.match(e.request)
-.then(res => {
-if (res) {
-//recuperar del cache
-return res
-}
-//recuperar de la petición a la url
-return fetch(e.request)
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
 })
-)
-});
-
-
-
-self.addEventListener("activate",
-  () =>
-    console.log("sw activo."));
-
-async function cargaCache() {
-  console.log(
-    "Intentando cargar cache",
-    CACHE);
-  const cache =
-    await caches.open(CACHE);
-  await cache.addAll(ARCHIVOS);
-  console.log("Cache", CACHE,
-    "cargado");
-}
-
-async function usaCache(evt) {
-  const cache =
-    await caches.open(CACHE);
-  const response =
-    await cache.match(evt.request,
-      { ignoreSearch: true });
-  if (response) {
-    return response;
-  } else {
-    return fetch(evt.request);
-  }
-}
