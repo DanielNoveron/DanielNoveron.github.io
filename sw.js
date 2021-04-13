f52c63b21ed7df6e668ed846adeb5a48593e6fcc
DanielNoveron <81577034+DanielNoveron@users.noreply.github.com>
2021-04-12 19:10:56 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -60,7 +60,7 @@ async function cargaCache() {
     "Intentando cargar cache",
     CACHE);
   const cache =
-    await caches.open(CACHE);
+    await cache.open(CACHE);
   await cache.addAll(ARCHIVOS);
   console.log("Cache", CACHE,
     "cargado");
